208dba4e5cb7befb91487266f91ea5b784a4d6fc
John Palmer <john_palmer@brown.edu>
2015-01-19 15:40:17 -0500

    added new republic spider

---
+++ crawlers/spiders/newRepublic_spider.py
@@ -0,0 +1,62 @@
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+
+from crawlers.items import newsItem
+import unicodedata
+
+class NewRepublic(CrawlSpider):
+    name = "newRepublic"
+    allowed_domains = ["newrepublic.com"]
+    start_urls = ["http://www.newrepublic.com/", "http://www.newrepublic.com/tags/politics", "http://www.newrepublic.com/tags/culture", "http://www.newrepublic.com/latest"]
+
+    rules = (
+        Rule(SgmlLinkExtractor(restrict_xpaths=['//*[@id="homepage"]/div[2]/div[3]/div/div/div/div/h3/a', '//*[@id="homepage"]/div[2]/div[1]/div[3]/div/div/h3/a', '//*[@id="homepage"]/div[2]/div[1]/div[2]/div/h2/a', '//*[@id="tag"]/div[2]/div/div/div[1]/div[1]/div/div[2]/h3/a', '//*[@id="latest"]/div[2]/div/div/div/div[1]/div/div[2]/h3/a']),callback='parse_item', follow=True),
+        )
+
+    def parse_item(self, response):
+        try:
+            sel = Selector(response)
+            #print response
+            title = sel.xpath('//*[@id="article"]/div[2]/div[1]/div[2]/div[1]/div/h1/span/text()')
+            #print '           '
+            #print unicodedata.normalize('NFKD', title[0].extract()).encode('ascii', 'ignore')
+            #print '           '
+            #try:
+            author = sel.xpath('//*[@id="article"]/div[2]/div[1]/div[2]/div[2]/div/div[2]/div[1]/h5/a/text()')
+            #    item['author'] = unicodedata.normalize('NFKD', author[0].extract()).encode('ascii', 'ignore')
+            #except:
+            #    item['author'] = ''
+                
+            #content = sel.xpath('//*[@id="story"]/p[1]/text()') #List
+            content = sel.xpath('//*[@id="article"]/div[2]/div[1]/div[2]/div[2]/div/div[2]/div[1]/div/p/text()')
+            link = sel.xpath('/html/head/link[6]/@href')
+
+
+            item = newsItem()
+
+            try:
+                date = sel.xpath('//*[@id="article"]/div[2]/div[1]/div[2]/div[1]/div/h5/span/text()')
+                item['date'] = unicodedata.normalize('NFKD', date[0].extract()).encode('ascii', 'ignore')
+            except:
+                item['date'] = -1
+
+
+            item['title'] = unicodedata.normalize('NFKD', title[0].extract()).encode('ascii', 'ignore')
+            item['link'] = unicodedata.normalize('NFKD', link[0].extract()).encode('ascii', 'ignore')
+            item['author'] = unicodedata.normalize('NFKD', author[0].extract()).encode('ascii', 'ignore')
+            item['publication'] = 'The Weekly Standard'
+            item['politicalScore'] = 0
+            item['posNegScore'] = 0
+            
+            wholeBody = ''
+            for part in content:
+                wholeBody += unicodedata.normalize('NFKD', part.extract()).encode('ascii', 'ignore')
+            item['body'] = unicodedata.normalize('NFKD', content[0].extract()).encode('ascii', 'ignore')
+            item['body'] = wholeBody
+            #print(link)
+
+            return [item]
+        except:
+            return []
